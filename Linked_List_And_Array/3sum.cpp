7a116e1e1013ad7c46a2c9324ecca80cceb6e778
Ashish Rath <ashishrath1997@gmail.com>
2022-06-30 00:02:35 +0530

    Create 3 Sum

--- Linked_List_And_Array/3sum.cpp
+++ Linked_List_And_Array/3sum.cpp
@@ -1,1 +1,28 @@
-
+#include <bits/stdc++.h> 
+vector<vector<int>> findTriplets(vector<int>arr, int n, int K) {
+    vector<vector<int>> ans;
+    if(n == 0) return ans;
+    sort(arr.begin(), arr.end());
+    for(int i = 0; i < n - 1; i++) {
+        int target = K - arr[i];
+        int l = i + 1;
+        int r = n - 1;
+        while(l < r) {
+            int sum = arr[l] + arr[r];
+            if(sum == target) {
+                ans.push_back({arr[i], arr[l], arr[r]});
+                int low = arr[l], high = arr[r] ;
+                while (l < r && arr[l] == low) l++ ;
+                while (l < r && arr[r] == high) r-- ;
+            } else if(sum < target) {
+                l++;
+            } else {
+                r--;
+            }
+        }
+        
+        while(i < n - 1 && arr[i] == arr[i + 1]) i++;
+    }
+    
+    return ans;
+}
